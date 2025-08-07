9292a65d9485f6594df50ec554837477f5907701
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-08-07 06:45:29 +0100

    Made the render method

--- peopleListObjectLiteralP/script.js
+++ peopleListObjectLiteralP/script.js
@@ -13,6 +13,23 @@ const peopleModule = {
         this.ulEl = peopleSection.querySelector("ul")
     },
 
+    render: function(){
+        this.ulEl.textContent = ""
+        this.people.forEach((name)=>{
+            const newName = document.createElement("span")
+            const newNameDel = document.createElement("button")
+            const newNameLi = document.createElement("li")
+
+            newName.textContent = name
+            newNameDel.textContent = "❌"
+
+            newNameLi.appendChild(newName)
+            newNameLi.appendChild(newNameDel)
+            this.ulEl.appendChild(newNameLi)
+
+        })
+    },
+
     bindEvents: function(){
         this.addBtn.addEventListener("click", this.addPerson.bind(this))
     },
@@ -20,6 +37,7 @@ const peopleModule = {
     addPerson: function(){
         this.people.push(this.inputEl.value)
         this.inputEl.value = ""
+        this.render()
     },
 }
 
