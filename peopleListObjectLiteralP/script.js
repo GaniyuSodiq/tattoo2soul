916eb3bd575654e978090ae7125b57b1294a0cb6
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-08-08 04:24:25 +0100

    inish the delete method

--- peopleListObjectLiteralP/script.js
+++ peopleListObjectLiteralP/script.js
@@ -1,5 +1,5 @@
 const peopleModule = {
-    people: ["Sodiq"],
+    people: [],
 
     init: function () {
         this.cacheDom()
@@ -8,22 +8,22 @@ const peopleModule = {
 
     cacheDom: function () {
         this.peopleSection = document.querySelector("#peopleSection")
-        this.addBtn = peopleSection.querySelector("button")
-        this.inputEl = peopleSection.querySelector("input")
-        this.ulEl = peopleSection.querySelector("ul")
-        this.delName = peopleSection.querySelector(".delName")
+        this.addBtn = this.peopleSection.querySelector("button")
+        this.inputEl = this.peopleSection.querySelector("input")
+        this.ulEl = this.peopleSection.querySelector("ul")
     },
 
-    render: function(){
+    render: function () {
         this.ulEl.textContent = ""
-        this.people.forEach((name)=>{
+        this.people.forEach((name) => {
             const newName = document.createElement("span")
             const newNameDel = document.createElement("button")
             const newNameLi = document.createElement("li")
 
             newName.textContent = name
             newNameDel.textContent = "❌"
-            newNameDel.classList.add("delName")
+            //  newNameDel.classList.add("delName")
+            //  this.delName = this.peopleSection.querySelector(".delName")
 
             newNameLi.appendChild(newName)
             newNameLi.appendChild(newNameDel)
@@ -32,21 +32,34 @@ const peopleModule = {
         })
     },
 
-    bindEvents: function(){
+    bindEvents: function () {
         this.addBtn.addEventListener("click", this.addPerson.bind(this))
-        this.delName.addEventListener("click", this.deletePerson.bind(this))
+        // this.delName.addEventListener("click", this.deletePerson.bind(this))
+        this.ulEl.addEventListener("click", this.deletePerson.bind(this))
     },
 
-    addPerson: function(){
+    addPerson: function () {
         this.people.push(this.inputEl.value)
         this.inputEl.value = ""
         this.render()
     },
 
-    deletePerson: function(event){
-        const nameToRemove = event.target.closest("li")
-        nameToRemove.remove()
-        console.log("hello test")
+    deletePerson: function (event) {
+        if (event.target.textContent == "❌") {
+            const nameToRemove = event.target.closest("li").querySelector("span").textContent
+            console.log(nameToRemove)
+            // the code below messed with me. it has to be IIFE. just decaration wont execute the fn, u knw that
+            const indexToRemove = (()=>{
+                for (let i = 0; i < this.people.length; i++){
+                    if (this.people[i] == nameToRemove){
+                        console.log(this.people[i])
+                        return i
+                    }
+                }
+            })()
+            this.people.splice(indexToRemove, 1)
+            this.render()
+        } else { return }
     },
 }
 
