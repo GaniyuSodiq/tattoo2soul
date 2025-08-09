90f92a019ae60e86eb21881506f8d64c47d55de5
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-08-09 09:02:21 +0100

    finish pubSub

---
+++ peopleListIIFE/pubSub.js
@@ -0,0 +1,32 @@
+const emit = (function () {
+    const emit = {}
+
+    function sub(arrayName, fn) {
+        emit[arrayName] = emit[arrayName] || []
+        emit[arrayName].push(fn)
+    }
+
+    function pub(arrayName, data) {
+        for (let key in emit) {
+            if (key = arrayName) {
+                emit[arrayName].forEach(fn => {
+                    fn(data)
+                });
+            }
+        }
+    }
+
+    function off(arrayName, fn) {
+        for (let key in emit) {
+            if (key = arrayName) {
+                for (let i = 0; i < emit[arrayName].lenght; i++) {
+                    if (emit[arrayName][i] = fn){
+                        emit[arrayName].splice(i, 1)
+                    }
+                }
+            }
+        }
+    }
+
+    return { sub, off, pub }
+})()
