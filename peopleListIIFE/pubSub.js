f5ff5879d04a7706f28f6c19d8b9af7842a001cf
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-08-10 08:06:27 +0100

    use for loop to go through the emit object

--- peopleListIIFE/pubSub.js
+++ peopleListIIFE/pubSub.js
@@ -8,43 +8,40 @@ const emit = (function () {
 
     function pub(arrayName, data) {
 
-        if (emit[arrayName]) {
-            emit[arrayName].forEach(fn => {
-                fn(data)
-            });
-        }
-
-
-        // for (let key in emit) {
-        //     if (key = arrayName) {
-        //         emit[arrayName].forEach(fn => {
-        //             fn(data)
-        //         });
-        //     }
+        // if (emit[arrayName]) {
+        //     emit[arrayName].forEach(fn => {
+        //         fn(data)
+        //     });
         // }
+
+        for (let key in emit) {
+            if (key = arrayName) {
+                emit[arrayName].forEach(fn => {
+                    fn(data)
+                });
+            }
+        }
     }
 
     function off(arrayName, fn) {
-        if (emit[arrayName]) {
-            for (let i = 0; i < emit[arrayName].lenght; i++) {
-                if (emit[arrayName][i] = fn) {
-                    emit[arrayName].splice(i, 1)
+        // if (emit[arrayName]) {
+        //     for (let i = 0; i < emit[arrayName].lenght; i++) {
+        //         if (emit[arrayName][i] = fn) {
+        //             emit[arrayName].splice(i, 1)
+        //         }
+        //     }
+        // }
+
+        for (let key in emit) {
+            if (key = arrayName) {
+                for (let i = 0; i < emit[arrayName].lenght; i++) {
+                    if (emit[arrayName][i] = fn) {
+                        emit[arrayName].splice(i, 1)
+                    }
                 }
             }
         }
-
     }
-
-    //     for (let key in emit) {
-    //         if (key = arrayName) {
-    //             for (let i = 0; i < emit[arrayName].lenght; i++) {
-    //                 if (emit[arrayName][i] = fn) {
-    //                     emit[arrayName].splice(i, 1)
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
 
     return { sub, off, pub }
 })()
