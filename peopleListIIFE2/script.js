f47df08b90b6e99042e715dc2402e84da7b7f5ed
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-08-12 09:02:54 +0100

    finish the main script js

--- peopleListIIFE2/script.js
+++ peopleListIIFE2/script.js
@@ -1,6 +1,5 @@
-const peopleModule = (function(){
+const peopleModule = (function () {
     const people = []
-    
     // c r i b
 
     // cacheDOM
@@ -11,9 +10,9 @@ const peopleModule = (function(){
 
     // render
 
-    function render (){
+    function render() {
         ulEl.textContent = ""
-        people.forEach(name=>{
+        people.forEach(name => {
             const newName = document.createElement("span")
             const newNameDel = document.createElement("button")
             const newNameLi = document.createElement("li")
@@ -31,24 +30,36 @@ const peopleModule = (function(){
     addBtn.addEventListener("click", addPerson)
     ulEl.addEventListener("click", deletePerson)
 
-    function addPerson(){
-        people.push(inputEl.value)
+    function addPerson(name) {
+        const value = typeof name == "string" ? name : inputEl.value
+        people.push(value)
         inputEl.value = ""
         render()
     }
 
-    function deletePerson(event){
-        if (event.target.textContent == "❌"){
-            const nameToRemove = event.target.closest("li").querySelector("span").textContent
-            const indexToRemove = (()=>{
-                for (let i=0; i<people.length; i++){
-                    if (people[i] == nameToRemove){
-                        return i
+    function deletePerson(event) {
+        if (typeof event == "number") {
+            people.splice(event, 1)
+        } else {
+            if (event.target.textContent == "❌") {
+                const nameToRemove = event.target.closest("li").querySelector("span").textContent
+                const indexToRemove = (() => {
+                    for (let i = 0; i < people.length; i++) {
+                        if (people[i] == nameToRemove) {
+                            return i
+                        }
                     }
-                }
-            })()
-            people.splice(indexToRemove, 1)
+                })()
+                people.splice(indexToRemove, 1)
+            }
         }
         render()
     }
+
+    return { addPerson, deletePerson }
 })()
+
+peopleModule.addPerson("Akeem")
+peopleModule.addPerson("Kareem")
+peopleModule.addPerson("Jimoh")
+peopleModule.deletePerson(0)
